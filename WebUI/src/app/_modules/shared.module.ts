c667a37ee783e1a0122646687f2c057dbfd58bba
Ahmed Muhammed Youssef <ahmed.muhammed.youssef@gmail.com>
2022-03-22 14:31:41 +0200

    Update Register Page
    Uses angular material design

--- WebUI/src/app/_modules/shared.module.ts
+++ WebUI/src/app/_modules/shared.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatTabsModule} from '@angular/material/tabs';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { ToastrModule } from 'ngx-toastr';
 import { TabsModule } from 'ngx-bootstrap/tabs'
@@ -8,6 +9,15 @@ import {BsDatepickerModule} from 'ngx-bootstrap/datepicker';
 import { PaginationModule } from 'ngx-bootstrap/pagination';
 import { TimeagoModule } from 'ngx-timeago';
 import { ButtonsModule } from 'ngx-bootstrap/buttons';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatInputModule} from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import {MatSelectModule} from '@angular/material/select';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @NgModule({
   declarations: [],
@@ -15,7 +25,10 @@ import { ButtonsModule } from 'ngx-bootstrap/buttons';
     CommonModule, BsDropdownModule.forRoot(),
     ToastrModule.forRoot({ positionClass: 'toast-bottom-right' }),
     TabsModule.forRoot(), FileUploadModule, BsDatepickerModule.forRoot(),
-    PaginationModule.forRoot(), TimeagoModule.forRoot(), ButtonsModule.forRoot()
+    PaginationModule.forRoot(), TimeagoModule.forRoot(), ButtonsModule.forRoot(),
+    MatTabsModule, MatButtonModule, 
+    MatFormFieldModule, MatIconModule, MatGridListModule, MatInputModule,
+    MatCardModule, MatSelectModule, MatDatepickerModule, MatNativeDateModule
   ],
   exports: [
     BsDropdownModule,
@@ -25,7 +38,17 @@ import { ButtonsModule } from 'ngx-bootstrap/buttons';
     BsDatepickerModule,
     PaginationModule,
     TimeagoModule,
-    ButtonsModule
+    ButtonsModule,
+    MatTabsModule, 
+    MatButtonModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatGridListModule,
+    MatInputModule,
+    MatCardModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatNativeDateModule
     ]
 })
 export class SharedModule { }
