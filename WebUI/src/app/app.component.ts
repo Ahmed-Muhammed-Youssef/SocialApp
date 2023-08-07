8ae0985bee7191ce3f67ff77ac0fa920cadb03d6
Ahmed Youssef <ahmed.muhammed.youssef@gmail.com>
2023-08-08 02:06:33 +0300

    Change website name to socail app

--- WebUI/src/app/app.component.ts
+++ WebUI/src/app/app.component.ts
@@ -10,7 +10,7 @@ import { PresenceService } from './_services/presence.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  public title = 'Dating App';
+  public title = 'Social App';
 
   isMobilePhone: boolean = false;
 
