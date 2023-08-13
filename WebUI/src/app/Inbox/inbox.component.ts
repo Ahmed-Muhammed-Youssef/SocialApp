abdaf2515c2510ac864895057c8489ef506b0f0b
Ahmed Youssef <ahmed.muhammed.youssef@gmail.com>
2023-08-13 18:49:29 +0300

    Fix: message button on the user card (#3)
    It now redirects to the inbox with the write chat open

--- WebUI/src/app/Inbox/inbox.component.ts
+++ WebUI/src/app/Inbox/inbox.component.ts
@@ -7,6 +7,7 @@ import { PresenceService } from '../_services/presence.service';
 import { UserService } from '../_services/user.service';
 import { BreakpointObserver } from '@angular/cdk/layout';
 import { ChatComponent } from '../chat/chat.component';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-inbox',
@@ -15,7 +16,7 @@ import { ChatComponent } from '../chat/chat.component';
 })
 export class InboxComponent implements OnInit {
   @ViewChild(ChatComponent) chatComponent!: ChatComponent;
-  currentUserSelection : User | undefined;
+  currentUserSelection: User | undefined;
   friendsPagination?: Pagination;
   friends: User[] | null = null;
   friendsPageNumber = 1;
@@ -25,7 +26,20 @@ export class InboxComponent implements OnInit {
   isMobilePhone: boolean = false;
 
   constructor(public messageService: MessageService, private userService: UserService,
-  public presenceService: PresenceService, private changeDetectorRef: ChangeDetectorRef, private breakpointObserver: BreakpointObserver) {
+    public presenceService: PresenceService, private changeDetectorRef: ChangeDetectorRef,
+    private breakpointObserver: BreakpointObserver, private route: ActivatedRoute) {
+    // Get username from url params
+    var username = this.route.snapshot.paramMap.get('username');
+    if (username) {
+      this.userService.getUserByUsername(username).subscribe(
+        response => {
+          if (response) {
+            this.currentUserSelection = response;
+          }
+        });
+    }
+
+    // Set break point 
     breakpointObserver.observe(["(max-width: 750px)"])
       .subscribe(
         result => {
@@ -34,21 +48,22 @@ export class InboxComponent implements OnInit {
             this.isMobilePhone = true;
           }
         }
-      ); }
+      );
+  }
 
   ngOnInit(): void {
     this.loadFriends();
   }
-  friendsPageChanged(e: any){
-    if(e && (e.pageIndex + 1) != this.friendsPageNumber){
+  friendsPageChanged(e: any) {
+    if (e && (e.pageIndex + 1) != this.friendsPageNumber) {
       this.friendsPageNumber = e.pageIndex + 1;
       this.loadFriends();
     }
   }
-  loadFriends(){
+  loadFriends() {
     this.userService.getMatches(this.friendsPageNumber, this.friendsPerPage).subscribe(
       r => {
-        if(r){
+        if (r) {
           this.friends = r.result;
           this.friendsPagination = r.pagination;
           // this.changeDetectorRef.detectChanges();
@@ -56,42 +71,42 @@ export class InboxComponent implements OnInit {
       }
     );
   }
-  loadChat(user: User){
+  loadChat(user: User) {
     this.currentUserSelection = user;
+    this.changeDetectorRef.detectChanges();
     this.chatComponent.loadChat();
   }
-  public getLoacaleDateTime(d: Date) : Date{
-    var localDate  = new Date(d.toString() + 'Z');
+  public getLoacaleDateTime(d: Date): Date {
+    var localDate = new Date(d.toString() + 'Z');
     return localDate;
   }
   public getDateTimeAgo(date: Date) {
     var now = new Date();
     date = this.getLoacaleDateTime(date);
     var yearDiff = now.getFullYear() - date.getFullYear();
-    var monthDiff = now.getMonth()- date.getMonth();
-    var dayDiff = now.getDate()- date.getDate();
+    var monthDiff = now.getMonth() - date.getMonth();
+    var dayDiff = now.getDate() - date.getDate();
     var hourDiff = now.getHours() - date.getHours();
     var minuteDiff = now.getMinutes() - date.getMinutes();
-    if(yearDiff > 0)
-    {
-      return yearDiff + ' year'+ (yearDiff> 1? 's':'') + ' ago';
+    if (yearDiff > 0) {
+      return yearDiff + ' year' + (yearDiff > 1 ? 's' : '') + ' ago';
     }
-    else 
-    if (monthDiff > 0){
-      return monthDiff + ' month'+ (monthDiff> 1? 's':'') + ' ago';
-    }
-    else if (dayDiff > 0){
-      return dayDiff + ' day'+ (dayDiff> 1? 's':'') + ' ago';
-    }
-    else if (hourDiff > 0){
-      return hourDiff + ' hour'+ (hourDiff> 1? 's':'') + ' ago';
-    }
-    else if (minuteDiff > 0){
-      return minuteDiff + ' minute'+ (minuteDiff> 1? 's':'') + ' ago';
-    }
-    else {
-      return 'online';
-    }
+    else
+      if (monthDiff > 0) {
+        return monthDiff + ' month' + (monthDiff > 1 ? 's' : '') + ' ago';
+      }
+      else if (dayDiff > 0) {
+        return dayDiff + ' day' + (dayDiff > 1 ? 's' : '') + ' ago';
+      }
+      else if (hourDiff > 0) {
+        return hourDiff + ' hour' + (hourDiff > 1 ? 's' : '') + ' ago';
+      }
+      else if (minuteDiff > 0) {
+        return minuteDiff + ' minute' + (minuteDiff > 1 ? 's' : '') + ' ago';
+      }
+      else {
+        return 'online';
+      }
   }
   ngOnDestroy(): void {
     this.messageService.stopHubConnection();
