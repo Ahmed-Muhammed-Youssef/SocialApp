81e372cf2922d8ab61f31f40075892298e6334b5
Ahmed Youssef <ahmed.muhammed.youssef@gmail.com>
2023-08-17 03:23:03 +0300

    Add: app user entity configurations

---
+++ API/Data/Configurations/AppUserConfigurations.cs
@@ -0,0 +1,34 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace API.Data.Configurations
+{
+    public class AppUserConfigurations : IEntityTypeConfiguration<AppUser>
+    {
+        public void Configure(EntityTypeBuilder<AppUser> builder)
+        {
+            // Key
+            builder.HasKey(u => u.Id);
+
+            // properties
+            builder.Property(u => u.FirstName).IsRequired().HasMaxLength(255);
+            builder.Property(u => u.LastName).IsRequired().HasMaxLength(255);
+            builder.Property(u => u.Sex).IsRequired();
+            builder.Property(u => u.Interest).IsRequired();
+            builder.Property(u => u.DateOfBirth).IsRequired();
+            builder.Property(u => u.Created).IsRequired();
+            builder.Property(u => u.LastActive).IsRequired();
+            builder.Property(u => u.City).IsRequired();
+            builder.Property(u => u.Country).IsRequired();
+
+            // relationships
+            // with user roles
+            builder.HasMany(u => u.UserRoles)
+               .WithOne(ur => ur.User)
+               .HasForeignKey(ur => ur.UserId)
+               .IsRequired()
+               .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,30 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Identity;
 
 namespace API.Entities
 {
     public class AppUser : IdentityUser<int>
     {
-        [Required]
-        [MaxLength(255)]
         public string FirstName { get; set; }
-        [Required]
-        [MaxLength(255)]
         public string LastName { get; set; }
-        [Required]
         public char Sex { get; set; }
-        [Required]
         public char Interest { get; set; } // can be f (female), m (male) or b (both)
-        [Required]
         public DateTime DateOfBirth { get; set; }
         public DateTime Created { get; set; } = DateTime.UtcNow;
         public DateTime LastActive { get; set; } = DateTime.UtcNow;
         public string Bio { get; set; }
-        [Required]
         public string City { get; set; }
-        [Required]
         public string Country { get; set; }
 
         // navigation properties
