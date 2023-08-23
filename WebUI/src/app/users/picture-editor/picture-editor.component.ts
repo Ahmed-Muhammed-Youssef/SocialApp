0889636a3b9c65bcfb05abd611dc637b79ef4629
Ahmed Youssef <ahmed.muhammed.youssef@gmail.com>
2023-08-23 23:08:49 +0300

    Add: picture preview to the picture upload

--- WebUI/src/app/users/picture-editor/picture-editor.component.ts
+++ WebUI/src/app/users/picture-editor/picture-editor.component.ts
@@ -8,6 +8,21 @@ import { PictureService } from 'src/app/_services/picture.service';
 })
 export class PictureEditorComponent implements OnInit {
   @Input() user: User | undefined = undefined;
+  imageInput = document.getElementById('imageInput');
+  imagePreviewUrl: string = "#";
   constructor(private imageService: PictureService) { }
   ngOnInit(): void {}
+  UpdatePreview(event : any)
+  {
+    if (event.target.files && event.target.files[0]) {
+      var reader = new FileReader();
+
+      reader.readAsDataURL(event.target.files[0]); // read file as data url
+
+      reader.onload = (event) => { // called once readAsDataURL is completed
+        if(event.target!.result)
+        this.imagePreviewUrl = event.target!.result as string;
+      }
+    }
+  }
 }
