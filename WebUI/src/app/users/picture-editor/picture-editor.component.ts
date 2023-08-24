81037aeb4876cd0c3a464ce0bcdac25043374b73
Ahmed Youssef <ahmed.muhammed.youssef@gmail.com>
2023-08-24 17:04:31 +0300

    Add: a functional upload picture component
    it also previews the image before uploading, still needs some styling and add progress tracker.

--- WebUI/src/app/users/picture-editor/picture-editor.component.ts
+++ WebUI/src/app/users/picture-editor/picture-editor.component.ts
@@ -1,4 +1,6 @@
+import { HttpEventType } from '@angular/common/http';
 import { Component, Input, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { User } from 'src/app/_models/User';
 import { PictureService } from 'src/app/_services/picture.service';
 @Component({
@@ -8,21 +10,38 @@ import { PictureService } from 'src/app/_services/picture.service';
 })
 export class PictureEditorComponent implements OnInit {
   @Input() user: User | undefined = undefined;
-  imageInput = document.getElementById('imageInput');
   imagePreviewUrl: string = "#";
-  constructor(private imageService: PictureService) { }
+  file: File | null = null;
+  imageUploadProgress: number = 0;
+  constructor(private imageService: PictureService, private toastr: ToastrService) { }
   ngOnInit(): void {}
-  UpdatePreview(event : any)
-  {
-    if (event.target.files && event.target.files[0]) {
-      var reader = new FileReader();
-
-      reader.readAsDataURL(event.target.files[0]); // read file as data url
-
-      reader.onload = (event) => { // called once readAsDataURL is completed
-        if(event.target!.result)
-        this.imagePreviewUrl = event.target!.result as string;
+  UploadImage(){
+    if(this.file)
+    {
+      this.imageService.uploadImage(this.file).subscribe(event => {
+        if (event.type === HttpEventType.UploadProgress){
+          this.imageUploadProgress = Math.round(100 * event.loaded / event.total);
+        }
+        else if(event.type === HttpEventType.Response){
+          this.toastr.success('Image uploaded successfully');
+          this.imagePreviewUrl = "#";
+        }
+      });
+    }
+  }
+  DisplayImage(e : any){
+    if (e.target.files && e.target.files[0]) {
+        this.file = e.target.files[0];
+        var reader = new FileReader();
+        reader.readAsDataURL(e.target.files[0]); // read file as data url
+        reader.onload = (event) => { // called once readAsDataURL is completed
+          if(event.target!.result)
+          this.imagePreviewUrl = event.target!.result as string;
       }
     }
+  }
+  CancelUpload(){
+    this.imagePreviewUrl = '#'
+    this.file = null;
   }
 }
