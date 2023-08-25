34837af0724117b99428c801bf5278d620a6d08b
Ahmed Youssef <ahmed.muhammed.youssef@gmail.com>
2023-08-25 05:35:26 +0300

    Add: the new pictures gallery (#13)

--- WebUI/src/app/users/gallery/gallery.component.ts
+++ WebUI/src/app/users/gallery/gallery.component.ts
@@ -1,15 +1,22 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { User } from 'src/app/_models/User';
+import { GalleryModule, GalleryItem, ImageItem } from 'ng-gallery';
 
 @Component({
   selector: 'app-gallery',
   templateUrl: './gallery.component.html',
-  styleUrls: ['./gallery.component.css']
+  styleUrls: ['./gallery.component.css'],
+  standalone: true,
+  imports: [GalleryModule]
 })
 export class GalleryComponent implements OnInit{
   @Input() user: User | undefined;
+  images: GalleryItem[] = [];
   ngOnInit(): void {
-    
+    if(this.user && this.user.pictures.length > 0)
+    {
+      this.user.pictures.forEach(p => this.images.push(new ImageItem({ src: p.url, thumb: p.url })))
+    }
   }
 
 }
