07454cd4863d2b303351f2f93883bf91aad592f5
Ahmed Youssef <ahmed.muhammed.youssef@gmail.com>
2023-08-31 20:07:52 +0300

    Add: a table of friend requests recieved (#35)

--- WebUI/src/app/friend-requests/friend-requests.component.ts
+++ WebUI/src/app/friend-requests/friend-requests.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FriendRequestsService } from '../_services/friend-requests.service';
+import { User } from '../_models/User';
+import { BreakpointObserver } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-friend-requests',
   templateUrl: './friend-requests.component.html',
   styleUrls: ['./friend-requests.component.css']
 })
-export class FriendRequestsComponent {
-
+export class FriendRequestsComponent implements OnInit {
+  friendRequests: User[] = [];
+  isMobilePhone = false;
+  constructor(private friendRequestsService: FriendRequestsService, breakpointObserver: BreakpointObserver){ 
+    breakpointObserver.observe(["(max-width: 750px)"])
+      .subscribe(
+        result => {
+          this.isMobilePhone = false;
+          if (result.matches) {
+            this.isMobilePhone = true;
+          }
+        }
+      );
+  }
+  ngOnInit(): void {
+    this.loadFriendRequests();
+  }
+  loadFriendRequests()
+  {
+    this.friendRequestsService.getFriendRequests().subscribe(
+      result => {
+        if(result) this.friendRequests = result;
+        console.log(result);
+      });
+  }
 }
