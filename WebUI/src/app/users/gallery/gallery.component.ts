e93be992a44ad6ff87db176f997b4b3f54adcf88
Ahmed Youssef <ahmed.muhammed.youssef@gmail.com>
2023-09-10 16:03:48 +0300

    Add: delete functionality to ui

--- WebUI/src/app/users/gallery/gallery.component.ts
+++ WebUI/src/app/users/gallery/gallery.component.ts
@@ -1,23 +1,32 @@
-import { ChangeDetectorRef, Component, Input, OnInit, Renderer2 } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { Picture } from 'src/app/_models/User';
+import { PictureService } from 'src/app/_services/picture.service';
 
 @Component({
   selector: 'app-gallery',
   templateUrl: './gallery.component.html',
   styleUrls: ['./gallery.component.css']
 })
-export class GalleryComponent implements OnInit{
+export class GalleryComponent implements OnInit {
   @Input() pictures: Picture[] = [];
   activeIndex = 0;
-  constructor(private changeDetectorRef: ChangeDetectorRef){}
+  constructor(private pictureSerive: PictureService, private toastr: ToastrService) { }
   ngOnInit(): void {
   }
   prevImage() {
     this.activeIndex = this.activeIndex > 0 ? this.activeIndex - 1 : this.pictures.length - 1;
   }
-  
+
   nextImage() {
     this.activeIndex = (this.activeIndex + 1) % this.pictures.length;
-    
+  }
+  deleteImage(pictureIndex: number) {
+    this.pictureSerive.deletePicture(this.pictures[pictureIndex].id).subscribe(r => {
+      if (r) {
+        this.toastr.success('Picture deleted successfully!');
+        this.pictures.splice(pictureIndex, 1);
+      }
+    });
   }
 }
