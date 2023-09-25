73851fcc93a719886e6ef7ba6af8fcb5223e4b3a
Ahmed Muhammed Youssef <ahmed.muhammed.youssef@gmail.com>
2023-09-25 16:11:54 +0300

    Remove: picture access data from user repo

--- API/Interfaces/IUsersRepository.cs
+++ API/Interfaces/IUsersRepository.cs
@@ -8,10 +8,8 @@ namespace API.Interfaces
 {
     public interface IUsersRepository
     {
-        public Task<Picture> AddPictureAsync(Picture picture);
         public Task<char> GetUserInterest(int userId);
         public void Update(AppUser appUser);
-        public void UpdatePicture(Picture picture);
         public Task<PagedList<UserDTO>> GetUsersDTOAsync(string username, UserParams userParams, List<int> ForbiddenIds);
         public Task<AppUser> GetUserByIdAsync(int id);
         public Task<UserDTO> GetUserDTOByIdAsync(int id);
@@ -20,9 +18,6 @@ namespace API.Interfaces
         public Task<UserDTO> GetUserDTOByEmailAsync(string email);
         public void DeleteUser(AppUser user);
         public Task<bool> UserExistsAsync(int id);
-        public Task<IEnumerable<PictureDTO>> GetUserPictureDTOsAsync(int id);
-        public Task<IEnumerable<Picture>> GetUserPictureAsync(int id);
         public Task<AppUser> GetUserByEmailAsync(string email);
-        public void DeletePicture(Picture picture);
     }
 }
