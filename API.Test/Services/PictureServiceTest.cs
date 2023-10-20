6457845e2e462cf326c73ea34ff88a97f015d361
Ahmed Muhammed Youssef <ahmed.muhammed.youssef@gmail.com>
2023-10-20 17:03:53 +0300

    Add: picture service tests (#51)

--- API.Test/Services/PictureServiceTest.cs
+++ API.Test/Services/PictureServiceTest.cs
@@ -1,10 +1,7 @@
 using API.Helpers;
 using API.Services;
-using CloudinaryDotNet;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Options;
-using NuGet.Configuration;
-using System.Configuration;
 
 namespace API.Test.Models
 {
@@ -13,11 +10,28 @@ namespace API.Test.Models
         private readonly PictureService _pictureService;
         public PictureServiceTest()
         {
+            var configuration = new ConfigurationBuilder()
+            .AddUserSecrets<PictureService>()
+            .Build();
+            CloudinarySettings cloudinarySettings = new CloudinarySettings() { 
+                CloudName = configuration["Cloudinary:CloudName"],
+                APIKey = configuration["Cloudinary:APIKey"],
+                APISecret = configuration["Cloudinary:APISecret"]
+            };
+            _pictureService = new PictureService(Options.Create(cloudinarySettings));
         }
         [Fact]
-        public void AddPictureAsync_Should_Throw_Exception_If_IForm_Is_Null()
+        public void AddPictureAsync_Should_Throw_NullReferenceException_If_IForm_Is_Null()
         {
-            
+            Assert.ThrowsAsync<NullReferenceException>(async () => await _pictureService.AddPictureAsync(null));
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        public void DeletePictureAsync_Should_Throw_Exception_If_ID_Is_Null_Or_Empty(string pictureId)
+        {
+            Assert.ThrowsAsync<NullReferenceException>(async () => await _pictureService.DeletePictureAsync(pictureId));
         }
     }
 }
