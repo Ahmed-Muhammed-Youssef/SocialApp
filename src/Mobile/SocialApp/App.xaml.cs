64139ee76e24c37fcee2121434c807a00370be27
Ahmed Muhammed Youssef <ahmed.muhammed.youssef@gmail.com>
2023-11-11 16:27:37 +0200

    Add: mobile application

---
+++ src/Mobile/SocialApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SocialApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
