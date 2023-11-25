db4d190bce1e90d6162a185f5249681d04b135ca
Ahmed Muhammed Youssef <ahmed.muhammed.youssef@gmail.com>
2023-11-25 15:28:01 +0200

    Add: folder structure to the mobile project

---
+++ src/Mobile/SocialApp/GlobalSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace SocialApp
+{
+    public class GlobalSettings
+    {
+        public const string DefaultEndpoint = "http://YOUR_IP_OR_DNS_NAME";
+        public const string ApplicationName = "SocialApp";
+    }
+}
