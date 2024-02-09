412a7f3c8c8a93a79447ea20ce41b10860702af4
Ahmed Muhammed Youssef <ahmed.muhammed.youssef@gmail.com>
2024-02-09 18:59:49 +0200

    Fix: stored procedure not working correctly

--- src/API/API.Infrastructure/Repositories/UserRepository.cs
+++ src/API/API.Infrastructure/Repositories/UserRepository.cs
@@ -41,10 +41,10 @@ namespace API.Infrastructure.Repositories
             {
                 var parameters = new DynamicParameters();
                 parameters.Add("@userId", userId);
-                parameters.Add("@sex", userParams.Sex);
+                parameters.Add("@sex", (int)userParams.Sex);
                 parameters.Add("@minAge", userParams.MinAge);
                 parameters.Add("@maxAge", userParams.MaxAge);
-                parameters.Add("@orderBy", userParams.OrderBy);
+                parameters.Add("@orderBy", (int)userParams.OrderBy);
                 parameters.Add("@pageNumber", userParams.PageNumber);
                 parameters.Add("@pageSize", userParams.ItemsPerPage);
 
