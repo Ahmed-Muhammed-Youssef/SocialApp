9f88ac4748f9c39684408ecb775a9382a102e8c3
Ahmed Youssef <ahmed.muhammed.youssef@gmail.com>
2024-02-11 11:50:50 +0200

    Remove: the Maui Application for now

--- src/Mobile/SocialApp/GlobalSettings.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SocialApp
-{
-    public class GlobalSettings
-    {
-        public const string DefaultEndpoint = "http://YOUR_IP_OR_DNS_NAME";
-        public const string ApplicationName = "SocialApp";
-    }
-}
