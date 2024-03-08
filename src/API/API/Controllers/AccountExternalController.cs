52710ea355dbb3b71c1782ff0cdd7cf5def00e76
Ahmed Muhammed Youssef <ahmed.muhammed.youssef@gmail.com>
2024-03-08 13:56:37 +0200

    Add: JWT token returned if google challenge is successfull (#75)

--- src/API/API/Controllers/AccountExternalController.cs
+++ src/API/API/Controllers/AccountExternalController.cs
@@ -5,13 +5,19 @@ using Microsoft.AspNetCore.Identity;
 using API.Application.Interfaces.Services;
 using API.Filters;
 using API.Application.Authentication.Google;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using API.Application.DTOs.Registeration;
+using API.Application.DTOs.User;
+using AutoMapper;
+using System;
 
 namespace API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
     [ServiceFilter(typeof(LogUserActivity))]
-    public class AccountExternalController(IGoogleAuthService _googleAuthService, UserManager<AppUser> userManager, ITokenService tokenService) : ControllerBase
+    public class AccountExternalController(IGoogleAuthService _googleAuthService, UserManager<AppUser> userManager, ITokenService tokenService, IMapper _mapper) : ControllerBase
     {
         // GET: api/AccountExternal/login-google
         [HttpGet("login-google")]
@@ -25,7 +31,21 @@ namespace API.Controllers
         public async Task<IActionResult> GoogleCallback(string code)
         {
             var userInfo = await _googleAuthService.GetUserFromGoogleAsync(code);
-            return Ok(userInfo); 
+
+            AppUser databaseUser = await userManager.Users.Where(u => u.Email == userInfo.Email).FirstOrDefaultAsync();
+
+            if (databaseUser is null)
+            {
+                // Create a new user
+                // But there is a problem with the unused username?
+                throw new NotImplementedException();
+            }
+
+            return Ok(new TokenDTO()
+            {
+                UserData = _mapper.Map<AppUser, UserDTO>(databaseUser),
+                Token = await tokenService.CreateTokenAsync(databaseUser)
+            });
         }
     }
 }
