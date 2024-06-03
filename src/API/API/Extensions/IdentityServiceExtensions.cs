74c72848125239c2b701dcf9f4c2443da13b12ac
Ahmed Youssef <ahmed.muhammed.youssef@gmail.com>
2024-06-03 23:06:14 +0300

    Add: Identity without affecting the existing authentication scheme (#78)
    the added razor pages need too many changes to work properly

--- src/API/API/Extensions/IdentityServiceExtensions.cs
+++ src/API/API/Extensions/IdentityServiceExtensions.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,7 +19,9 @@ namespace API.Extensions
             services.AddIdentityCore<AppUser>(opt =>
             {
                 opt.Password.RequireNonAlphanumeric = false;
+                // opt.SignIn.RequireConfirmedAccount = true;
             })
+                .AddDefaultUI()
                 .AddRoles<AppRole>()
                 .AddRoleManager<RoleManager<AppRole>>()
                 .AddSignInManager<SignInManager<AppUser>>()
@@ -27,7 +30,12 @@ namespace API.Extensions
             services.AddAuthorizationBuilder()
                 .AddPolicy("RequireAdminRole", policy => policy.RequireRole("admin"))
                 .AddPolicy("RequireModeratorOrAdmin", policy => policy.RequireRole("admin", "moderator"));
-            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
+            })
             .AddJwtBearer(options =>
             {
                 options.TokenValidationParameters = new TokenValidationParameters
