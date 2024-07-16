eff0f9c117ed9efa6a80f1e2e4dec65847d628b3
Ahmed Youssef <ahmed.muhammed.youssef@gmail.com>
2024-07-16 23:17:47 +0300

    feat: add simple nav bar #82

--- src/Frontend/AngularProject/src/app/app.component.ts
+++ src/Frontend/AngularProject/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MainBarComponent} from './main-bar/main-bar.component'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MainBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
