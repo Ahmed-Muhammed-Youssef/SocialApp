328e5baec493faece40b70a4558b744422e08659
Ahmed Youssef <ahmed.muhammed.youssef@gmail.com>
2024-08-19 00:13:23 +0300

    add: connection strings in the infrastructure #83

---
+++ src/WebApp/Common/Infrastructure/AppSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure
+{
+    internal static class AppSettings
+    {
+        public static string DefaultConnectionString { get; } = "add your connection string here";
+        public static string ProductionConnectionString { get; } = "add your connection string here";
+    }
+}
