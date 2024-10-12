550a989044974e2f7bd3aa7581f952fc2aba40d0
Ahmed Muhammed Youssef <ahmed.muhammed.youssef@gmail.com>
2024-10-12 15:37:42 +0300

    fix: remove aplication user's interest column #86

--- src/WebApp/Common/Domain/Entities/ApplicationUser.cs
+++ src/WebApp/Common/Domain/Entities/ApplicationUser.cs
@@ -8,7 +8,6 @@
         public string LastName { get; set; }
         public string ProfilePictureUrl { get; set; } = null;
         public char Sex { get; set; }
-        public char Interest { get; set; } // can be f (female), m (male) or b (both)
         public DateTime DateOfBirth { get; set; }
         public DateTime Created { get; set; } = DateTime.UtcNow;
         public DateTime LastActive { get; set; } = DateTime.UtcNow;
