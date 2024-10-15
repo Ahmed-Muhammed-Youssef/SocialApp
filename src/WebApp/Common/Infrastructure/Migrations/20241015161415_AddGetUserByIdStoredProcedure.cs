06dd7fdcf970ca8c883f93686a716f1dfe99fc3a
Ahmed Youssef <ahmed.muhammed.youssef@gmail.com>
2024-10-15 19:22:00 +0300

    add: GetUserById stored procedure #86

---
+++ src/WebApp/Common/Infrastructure/Migrations/20241015161415_AddGetUserByIdStoredProcedure.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddGetUserByIdStoredProcedure : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var sp = @"
+                        CREATE PROCEDURE GetUserById
+                            @id int
+                        AS
+                        BEGIN
+                            SET NOCOUNT ON;
+                            SELECT * 
+                            FROM dbo.AspNetUsers
+                            WHERE Id = @id;
+                        END";
+
+            migrationBuilder.Sql(sp);
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP PROCEDURE GetUserById");
+        }
+    }
+}
