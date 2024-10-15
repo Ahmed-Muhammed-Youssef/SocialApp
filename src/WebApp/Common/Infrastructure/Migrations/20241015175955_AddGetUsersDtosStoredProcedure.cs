6c0b53a1d94e76605f9eb3a3a9f4b6723b58c44d
Ahmed Youssef <ahmed.muhammed.youssef@gmail.com>
2024-10-15 21:02:30 +0300

    add: get users dtos stored procedure #86

---
+++ src/WebApp/Common/Infrastructure/Migrations/20241015175955_AddGetUsersDtosStoredProcedure.cs
@@ -0,0 +1,101 @@
+﻿using CloudinaryDotNet.Actions;
+using CloudinaryDotNet;
+using Domain.Entities;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.EntityFrameworkCore.Migrations;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddGetUsersDtosStoredProcedure : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"CREATE PROCEDURE[dbo].[GetUsersDtos]
+    @userId INT,
+            @minAge INT,
+    @maxAge INT,
+            @orderBy SMALLINT,
+            @pageNumber INT,
+    @pageSize INT
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+            --take all the forbidden ids in this table
+            DECLARE @ForbiddenIdsTable TABLE(Id INT);
+
+            --get friend requested users
+            INSERT INTO @ForbiddenIdsTable
+
+    SELECT U.Id
+            FROM FriendRequests AS FR
+    INNER JOIN dbo.ApplicationUsers AS U ON FR.RequestedId = U.Id
+    WHERE RequesterId = @userId;
+
+            --get the user friends
+            INSERT INTO @ForbiddenIdsTable
+
+    SELECT FriendId
+
+    FROM dbo.Friends
+    WHERE UserId = @userId;
+
+            --Calculate the date of birth ranges based on the age parameters
+    DECLARE @maxDoB AS DATETIME;
+            DECLARE @minDoB AS DATETIME;
+
+            IF NOT(@maxAge IS NULL)
+    BEGIN
+        SET @minDoB = DATEADD(YEAR, -@maxAge - 1, GETUTCDATE());
+            END
+
+            IF NOT(@minAge IS NULL)
+    BEGIN
+        SET @maxDoB = DATEADD(YEAR, -@minAge, GETUTCDATE());
+            END
+
+            -- Calculate the number of rows to skip
+            DECLARE @skip INT = (@pageNumber - 1) * @pageSize;
+
+            --Build the query
+    SELECT U.Id,
+		U.FirstName,
+		U.LastName,
+		U.ProfilePictureUrl,
+		U.Sex,
+		CONVERT(int, ROUND(DATEDIFF(hour, U.DateOfBirth, GETDATE()) / 8766.0, 0)) As 'Age',
+		U.Created,
+		U.LastActive,
+		U.Bio,
+		U.CityId
+    FROM dbo.ApplicationUsers AS U
+    WHERE Id != @userId
+    AND(@minAge IS NULL OR DateOfBirth <= @maxDoB)
+    AND(@maxAge IS NULL OR DateOfBirth >= @minDoB)
+    AND Id NOT IN(SELECT Id FROM @ForbiddenIdsTable)
+    ORDER BY
+        CASE @orderBy
+            WHEN 2 THEN Created
+            WHEN 1 THEN DateOfBirth
+            ELSE LastActive
+        END DESC
+    OFFSET @skip ROWS
+    FETCH NEXT @pageSize ROWS ONLY;
+            END");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP PROCEDURE GetUsersDtos");
+        }
+    }
+}
