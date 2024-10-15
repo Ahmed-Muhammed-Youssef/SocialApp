6d5130e8e3d6f3b375491e6fc0cb31a6d0d48318
Ahmed Youssef <ahmed.muhammed.youssef@gmail.com>
2024-10-15 20:29:12 +0300

    add: basic missing stored procedures #86

---
+++ src/WebApp/Common/Infrastructure/Migrations/20241015171929_AddGetUserByIdStoredProcedure.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddGetUserByIdStoredProcedure : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"CREATE PROCEDURE GetUserById
+    @id int
+AS
+BEGIN
+    SET NOCOUNT ON;
+            SELECT*
+            FROM dbo.ApplicationUsers
+            WHERE Id = @id;
+END");
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP PROCEDURE GetUserById");
+        }
+    }
+}
---
+++ src/WebApp/Common/Infrastructure/Migrations/20241015172148_AdGetFriendRequestedUsersDTOStoredProcedure.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdGetFriendRequestedUsersDTOStoredProcedure : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"CREATE PROCEDURE GetFriendRequestedUsersDTO
+    @senderId INT
+AS
+BEGIN
+    SELECT U.Id,
+		U.FirstName,
+		U.LastName,
+		U.ProfilePictureUrl,
+		U.Sex,
+		CONVERT(int, ROUND(DATEDIFF(hour,U.DateOfBirth,GETDATE())/8766.0,0)) As 'Age',
+		U.Created,
+		U.LastActive,
+		U.Bio,
+		U.CityId
+    FROM FriendRequests AS FR
+	INNER JOIN dbo.ApplicationUsers AS U ON FR.RequestedId = U.Id 
+    WHERE RequesterId = @senderId;
+END");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP PROCEDURE GetFriendRequestedUsersDTO");
+        }
+    }
+}
