a7ee8aa5b64b2ca6da6a6f36c0a57aeaa135dae3
Ahmed Youssef <ahmed.muhammed.youssef@gmail.com>
2024-10-15 20:29:12 +0300

    add: basic missing stored procedures #86

---
+++ src/WebApp/Common/Infrastructure/Migrations/20241015172653_AddGetUserDtoByIdtoredProcedure.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddGetUserDtoByIdtoredProcedure : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"CREATE PROCEDURE GetUserDtoById
+	@id int
+AS
+BEGIN
+	SET NOCOUNT ON;
+	SELECT U.Id,
+		U.FirstName,
+		U.LastName,
+		U.ProfilePictureUrl,
+		U.Sex,
+		CONVERT(int, ROUND(DATEDIFF(hour,U.DateOfBirth,GETDATE())/8766.0,0)) As 'Age',
+		U.Created,
+		U.LastActive,
+		U.Bio,
+		U.CityId 
+	FROM dbo.ApplicationUsers AS U
+	WHERE Id = @id;
+END");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP PROCEDURE GetUserDtoById");
+        }
+    }
+}
