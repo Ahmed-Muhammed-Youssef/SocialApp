799629663c26a2e1ed3c6f537d015afdf618beca
Ahmed Muhammed Youssef <ahmed.muhammed.youssef@gmail.com>
2024-10-25 14:17:49 +0300

    add: a shared library for cross cutting concerns #88

---
+++ src/WebApp/Common/Shared/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace Shared.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static string ToRelativeTimeString(this DateTime dateTime)
+        {
+            var timeDiff = DateTime.Now - dateTime;
+
+            if (timeDiff.TotalMinutes < 1)
+                return "just now";
+            if (timeDiff.TotalHours < 1)
+                return $"{(int)timeDiff.TotalMinutes}m ago";
+            if (timeDiff.TotalDays < 1)
+                return $"{(int)timeDiff.TotalHours}h ago";
+            if (timeDiff.TotalDays < 7)
+                return $"{(int)timeDiff.TotalDays}d ago";
+
+            return dateTime.ToString("MMM dd");
+        }
+    }
+}
