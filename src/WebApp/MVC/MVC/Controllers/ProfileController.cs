076c315311717cdf95f1384aaa64c75e59a47ae8
Ahmed Muhammed Youssef <ahmed.muhammed.youssef@gmail.com>
2024-10-26 16:13:46 +0300

    add: basic profile page #90

---
+++ src/WebApp/MVC/MVC/Controllers/ProfileController.cs
@@ -0,0 +1,26 @@
+﻿using Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVC.Controllers
+{
+    public class ProfileController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ProfileController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet("[controller]/{id}")]
+        public async Task<IActionResult> Index(int id)
+        {
+            Application.DTOs.User.UserDTO userProfile = await _unitOfWork.ApplicationUserRepository.GetDtoByIdAsync(id);
+            if (userProfile == null)
+            {
+                return NotFound();
+            }
+            return View(userProfile);
+        }
+    }
+}
