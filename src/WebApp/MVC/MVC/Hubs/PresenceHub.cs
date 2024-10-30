478bb0d697faeb80bce011542092536d8ea47f5a
Ahmed Youssef <ahmed.muhammed.youssef@gmail.com>
2024-10-30 22:21:25 +0300

    add: empty presenceHub #92

---
+++ src/WebApp/MVC/MVC/Hubs/PresenceHub.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+
+namespace MVC.Hubs
+{
+    [Authorize]
+    public class PresenceHub : Hub
+    {
+        public override Task OnConnectedAsync()
+        {
+            return base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception? exception)
+        {
+            return base.OnDisconnectedAsync(exception);
+        }
+    }
+}
