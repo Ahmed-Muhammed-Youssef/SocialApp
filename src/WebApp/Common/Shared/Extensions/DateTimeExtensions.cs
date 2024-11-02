6318456a403626017d61c9d2fb0fb2f277440d6f
Ahmed Muhammed Youssef <ahmed.muhammed.youssef@gmail.com>
2024-11-02 11:43:44 +0200

    refact: move datetime extenstion to the right layer #93

--- src/WebApp/Common/Shared/Extensions/DateTimeExtensions.cs
+++ src/WebApp/Common/Shared/Extensions/DateTimeExtensions.cs
@@ -2,6 +2,17 @@
 {
     public static class DateTimeExtensions
     {
+        public static int CalculateAge(this DateTime dob)
+        {
+            DateTime now = DateTime.Now;
+            int age = now.Year - dob.Year;
+            // we need this for leap years 
+            if (dob > now.AddYears(-age))
+            {
+                age--;
+            }
+            return age;
+        }
         public static string ToRelativeTimeString(this DateTime dateTime)
         {
             var timeDiff = DateTime.Now - dateTime;
