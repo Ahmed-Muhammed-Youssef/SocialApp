a645677864780f0b78bf4c33506a719ccd869dca
Ahmed Muhammed Youssef <ahmed.muhammed.youssef@gmail.com>
2024-11-14 19:33:08 +0200

    refact: move signalR logic into the site.js file #92

--- src/WebApp/MVC/MVC/wwwroot/js/site.js
+++ src/WebApp/MVC/MVC/wwwroot/js/site.js
@@ -1,4 +1,34 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿
 
-// Write your JavaScript code.
+function startSignalR() {
+    // Create SignalR connection
+    const connection = new signalR.HubConnectionBuilder()
+        .withUrl("/hubs/presence", { withCredentials: true })
+        .withAutomaticReconnect()
+        .build();
+
+    // Handle initial users list
+    connection.on("GetOnlineUsers", (users) => {
+        onlineUsers.push(...users);
+        document.dispatchEvent(new CustomEvent("GetOnlineUsers"));
+    });
+
+    // Handle new user coming online
+    connection.on("UserIsOnline", (user) => {
+        onlineUsers.push(user);
+        document.dispatchEvent(new CustomEvent("UserIsOnline"));
+    });
+
+    // Handle user going offline
+    connection.on("UserIsOffline", (userId) => {
+        const index = onlineUsers.findIndex(u => u.id === userId);
+        if (index !== -1) {
+            onlineUsers.splice(index, 1);
+        }
+        document.dispatchEvent(new CustomEvent("UserIsOffline"));
+    });
+
+    // Start the connection
+    connection.start()
+        .catch(err => console.error(err));
+}
