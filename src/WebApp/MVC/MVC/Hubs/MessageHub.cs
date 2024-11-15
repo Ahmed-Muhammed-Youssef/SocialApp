6326030413c60e2f84461903287a8b789115ecc1
Ahmed Muhammed Youssef <ahmed.muhammed.youssef@gmail.com>
2024-11-15 15:27:19 +0200

    feat: add the ability to send messages #92

---
+++ src/WebApp/MVC/MVC/Hubs/MessageHub.cs
@@ -0,0 +1,71 @@
+﻿
+using Application.DTOs.Message;
+using Application.Interfaces;
+using Domain.Entities;
+using Microsoft.AspNetCore.SignalR;
+using Shared.Extensions;
+
+namespace MVC.Hubs
+{
+    public class MessageHub : Hub
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public MessageHub(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public override Task OnConnectedAsync()
+        {
+            return base.OnConnectedAsync();
+        }
+        public async Task SendMessages(NewMessageDTO message)
+        {
+            var sender = await _unitOfWork.ApplicationUserRepository.GetByIdAsync(Context.User.GetPublicId().Value);
+            var recipient = await _unitOfWork.ApplicationUserRepository.GetByIdAsync(message.RecipientId);
+
+            if (recipient == null || sender == null)
+            {
+                throw new HubException("User not found");
+            }
+            if (sender.Id == recipient.Id)
+            {
+                throw new HubException("You can't send messages to yourself");
+            }
+            if (!await _unitOfWork.FriendRequestRepository.IsFriend(sender.Id, recipient.Id))
+            {
+                throw new HubException("You can send messages to friends only");
+
+            }
+            var createdMessage = new Message
+            {
+                SenderId = sender.Id,
+                RecipientId = recipient.Id,
+                Content = message.Content,
+                SenderDeleted = false,
+                RecipientDeleted = false,
+                ReadDate = null
+            };
+
+            await _unitOfWork.MessageRepository.AddMessageAsync(createdMessage);
+
+
+            if (await _unitOfWork.SaveChangesAsync())
+            {
+                MessageDTO msgDTO = new()
+                {
+                    Id = createdMessage.Id,
+                    SenderId = createdMessage.SenderId,
+                    RecipientId = createdMessage.RecipientId,
+                    Content = createdMessage.Content,
+                    SenderPhotoUrl = sender.ProfilePictureUrl
+                };
+                await Clients.User(msgDTO.RecipientId.ToString()).SendAsync("NewMessage", msgDTO);
+            }
+            else
+            {
+                throw new HubException("Couldn't Send the message");
+            }
+        }
+    }
+}
