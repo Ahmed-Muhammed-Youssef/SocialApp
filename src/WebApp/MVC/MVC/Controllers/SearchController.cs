328ad3fd84774c336099388d7ccb2c8d7c7758ef
Ahmed Youssef <ahmed.muhammed.youssef@gmail.com>
2024-11-18 21:51:18 +0200

    feat: add a control to display search results #103

---
+++ src/WebApp/MVC/MVC/Controllers/SearchController.cs
@@ -0,0 +1,30 @@
+﻿using Application.DTOs.Pagination;
+using Application.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Shared.Extensions;
+
+namespace MVC.Controllers
+{
+    [Authorize]
+    public class SearchController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public SearchController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public async Task<IActionResult> UsersAsync(string search, UserParams userParams)
+        {
+            int? publicId = User.GetPublicId();
+            if (publicId is not null)
+            {
+                var users = await _unitOfWork.ApplicationUserRepository.SearchAsync(publicId.Value, search, userParams);
+                return View(users);
+            }
+
+            return View();
+        }
+    }
+}
