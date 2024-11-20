39ae2180898c0a3a69adac6741a5fa27a209ffe3
Ahmed Youssef <ahmed.muhammed.youssef@gmail.com>
2024-11-20 21:28:09 +0200

    feat: include the user realtion data #103

---
+++ src/WebApp/Common/Application/DTOs/User/UserProfileDTO.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Constants;
+
+namespace Application.DTOs.User
+{
+    public class UserProfileDTO
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string ProfilePictureUrl { get; set; }
+        public char Sex { get; set; }
+        public int Age { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime LastActive { get; set; }
+        public string Bio { get; set; }
+        public SocialRelation Relation { get; set; }
+    }
+}
---
+++ src/WebApp/Common/Domain/Constants/SocialRelation.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Constants
+{
+    public enum SocialRelation
+    {
+        Friend,
+        NotFriend,
+        FrinedRequesSent,
+        FriendRequestReceived
+    }
+}
