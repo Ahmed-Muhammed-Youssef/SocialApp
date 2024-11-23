819bfa0a5e6ad36bfb07ef713a5d27bc825be596
Ahmed Muhammed Youssef <ahmed.muhammed.youssef@gmail.com>
2024-11-23 13:25:03 +0200

    feat: add interceptors #82

---
+++ src/Frontend/SocialApp-Angular/src/app/_interceptors/loading.interceptor.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable, finalize } from 'rxjs';
+import { BusyService } from '../_services/busy.service';
+
+@Injectable()
+export class LoadingInterceptor implements HttpInterceptor {
+
+  constructor(private busyService: BusyService) {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    this.busyService.busy();
+    return next.handle(request).pipe(
+      finalize(() => this.busyService.idle())
+    );
+  }
+}
