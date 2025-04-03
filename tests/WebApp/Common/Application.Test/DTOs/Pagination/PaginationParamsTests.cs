3a161903f7a81345e136d2e8af5b12e0ccc0ea05
Ahmed Muhammed Youssef <ahmed.muhammed.youssef@gmail.com>
2025-04-03 16:21:42 +0200

    tests: add paginationParams tests

---
+++ tests/WebApp/Common/Application.Test/DTOs/Pagination/PaginationParamsTests.cs
@@ -0,0 +1,62 @@
+﻿using Application.DTOs.Pagination;
+using Domain.Constants;
+
+namespace Application.Test.DTOs.Pagination
+{
+    public class PaginationParamsTests
+    {
+        [Fact]
+        public void DefaultValues()
+        {
+            // Arrange & Act
+            PaginationParams paginationParams = new();
+
+            // Assert
+            Assert.Equal(0, paginationParams.SkipValue);
+            Assert.Equal(1, paginationParams.PageNumber);
+            Assert.Equal(10, paginationParams.ItemsPerPage);
+        }
+
+        [Fact]
+        public void Invalid_ItemsPerPage_Uses_PageSizeMax()
+        {
+            // Arrange
+            PaginationParams paginationParams = new()
+            {
+                // Act
+                ItemsPerPage = -1
+            };
+
+            // Assert
+            Assert.Equal(SystemPolicy.MaxPageSize, paginationParams.ItemsPerPage);
+        }
+
+        [Fact]
+        public void ItemsPerPage_Bigger_Than_PageSizeMax_Uses_PageSizeMax()
+        {
+            // Arrange
+            PaginationParams paginationParams = new()
+            {
+                // Act
+                ItemsPerPage = SystemPolicy.MaxPageSize + 1
+            };
+
+            // Assert
+            Assert.Equal(SystemPolicy.MaxPageSize, paginationParams.ItemsPerPage);
+        }
+
+        [Fact]
+        public void Invalid_PageNumber_Ignored()
+        {
+            // Arrange
+            PaginationParams paginationParams = new()
+            {
+                // Act
+                PageNumber = -1
+            };
+
+            // Assert
+            Assert.Equal(1, paginationParams.PageNumber);
+        }
+    }
+}
