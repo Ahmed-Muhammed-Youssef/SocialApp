1a0456fc97de2d948d7f96fd977f71d4bb1cd3ef
Ahmed Muhammed Youssef <ahmed.muhammed.youssef@gmail.com>
2025-04-03 17:17:30 +0200

    tests: add user params tests

---
+++ tests/WebApp/Common/Application.Test/DTOs/Pagination/UserParamsTests.cs
@@ -0,0 +1,36 @@
+﻿using Application.DTOs.Pagination;
+using Domain.Constants;
+using Domain.Enums;
+
+namespace Application.Test.DTOs.Pagination
+{
+    public class UserParamsTests
+    {
+        [Fact]
+        public void Constructor_DefaultInput_SetsPropertiesCorrectly()
+        {
+            // Arrange
+            UserParams userParams = new UserParams();
+            // Act
+
+            // Assert
+            Assert.Equal(SystemPolicy.UsersMinimumAge, userParams.MinAge);
+            Assert.Equal(OrderByOptions.LastActive, userParams.OrderBy);
+            Assert.Null(userParams.MaxAge);
+        }
+
+        [Fact]
+        public void MinAge_InvalidInput_Ignored()
+        {
+            // Arrange
+            UserParams userParams = new()
+            {
+                // Act
+                MinAge = SystemPolicy.UsersMinimumAge - 1
+            };
+
+            // Assert
+            Assert.Equal(SystemPolicy.UsersMinimumAge, userParams.MinAge);
+        }
+    }
+}
