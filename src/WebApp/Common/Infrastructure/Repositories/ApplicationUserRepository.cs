4e36f1c1a2931657b4d21cbc52efa57bae4e7c63
Ahmed Youssef <ahmed.muhammed.youssef@gmail.com>
2025-10-27 15:11:57 +0300

    feat: use ef instead of sp to get users #117

--- src/WebApp/Common/Infrastructure/Repositories/ApplicationUserRepository.cs
+++ src/WebApp/Common/Infrastructure/Repositories/ApplicationUserRepository.cs
@@ -9,6 +9,9 @@ using Application.DTOs.User;
 using Application.DTOs.Pagination;
 using AutoMapper;
 using Application.Features.Users;
+using Domain.Enums;
+using Shared.Extensions;
+using Application.DTOs.Picture;
 
 namespace Infrastructure.Repositories;
 
@@ -30,34 +33,83 @@ public class ApplicationUserRepository(DataContext _dataContext, IMapper mapper)
             .AsNoTracking()
             .AnyAsync(e => e.Id == id);
     }
+    
     public void Update(ApplicationUser appUser)
     {
         _dataContext.ChangeTracker.Clear();
         _dataContext.Entry(appUser).State = EntityState.Modified;
     }
+
     public async Task<PagedList<UserDTO>> GetUsersDTOAsync(int userId, UserParams userParams)
     {
-        IEnumerable<UserDTO> users = [];
+        DateTime? birthDateMax = userParams.MaxAge != null ? DateTime.UtcNow.AddYears(-userParams.MaxAge.Value - 1) : null;
+        DateTime birthDateMin = DateTime.UtcNow.AddYears(-userParams.MinAge - 1);
 
-        using (var connection = new SqlConnection(_dataContext.Database.GetDbConnection().ConnectionString))
+        IQueryable<ApplicationUser> query = _dataContext.ApplicationUsers
+            .Include(u => u.Friends)
+            .Include(u => u.FriendRequestsReceived)
+            .Where(u => u.Id != userId && u.DateOfBirth >= birthDateMin && (birthDateMax == null || u.DateOfBirth <= birthDateMax))
+            .AsQueryable();
+
+        // filteration based on relation
+        if (userParams.RelationFilter == RelationFilter.OnlyFriends)
         {
-            var parameters = new DynamicParameters();
-            parameters.Add("@userId", userId);
-            parameters.Add("@minAge", userParams.MinAge);
-            parameters.Add("@maxAge", userParams.MaxAge);
-            parameters.Add("@orderBy", (int)userParams.OrderBy);
-            parameters.Add("@pageNumber", userParams.PageNumber);
-            parameters.Add("@pageSize", userParams.ItemsPerPage);
-
-            users = await connection.QueryAsync<UserDTO>("GetUsersDtos", parameters, commandType: CommandType.StoredProcedure);
+            query = query.Where(u => u.Friends.Any(f => f.FriendId == userId));
         }
-        return new PagedList<UserDTO>(users.ToList(), users.Count(), userParams.PageNumber, userParams.ItemsPerPage);
+        else if (userParams.RelationFilter == RelationFilter.OnlyFriendRequested)
+        {
+            query = query.Where(u => u.FriendRequestsReceived.Any(fr => fr.RequesterId == userId));
+        }
+        else if (userParams.RelationFilter == RelationFilter.ExcludeFriendsAndFriendRequested)
+        {
+            query = query.Where(u => !u.Friends.Any(f => f.FriendId == userId) &&
+                                     !u.FriendRequestsReceived.Any(fr => fr.RequesterId == userId));
+        }
+
+        // ordering
+        query = userParams.OrderBy switch
+        {
+            OrderByOptions.CreationTime => query.OrderByDescending(u => u.Created),
+            OrderByOptions.LastActive => query.OrderByDescending(u => u.LastActive),
+            OrderByOptions.Age => query.OrderByDescending(u => u.DateOfBirth),
+            _ => query.OrderByDescending(u => u.LastActive)
+        };
+
+        // projection
+        var projectedQuery = query.Select(u => new UserDTO
+        {
+            Id = u.Id,
+            FirstName = u.FirstName,
+            LastName = u.LastName,
+            ProfilePictureUrl = u.ProfilePictureUrl,
+            Sex = u.Sex,
+            Age = u.DateOfBirth.CalculateAge(),
+            Created = u.Created,
+            LastActive = u.LastActive,
+            Bio = u.Bio ?? string.Empty,
+            Pictures = u.Pictures.Select(p => new PictureDTO
+            {
+                Id = p.Id,
+                Url = p.Url
+            })
+        });
+
+        var count = await projectedQuery.CountAsync();
+
+        // pagination and execution
+        List<UserDTO> users = await projectedQuery
+            .Skip(userParams.SkipValue())
+            .Take(userParams.ItemsPerPage)
+            .ToListAsync();
+
+        return new PagedList<UserDTO>(users, count, userParams.PageNumber, userParams.ItemsPerPage);
     }
+
     public async Task<ApplicationUser?> GetByIdentity(string identity)
     {
         return await _dataContext.ApplicationUsers.FirstOrDefaultAsync(u => u.IdentityId == identity);
     }
-    
+
     public async Task<ApplicationUser?> GetByIdAsync(int id)
     {
         var connectionString = _dataContext.Database.GetConnectionString();
@@ -81,7 +133,7 @@ public class ApplicationUserRepository(DataContext _dataContext, IMapper mapper)
 
     public async Task<List<SimplifiedUserDTO>> GetListAsync(int[] ids)
     {
-        return await _dataContext.ApplicationUsers.Where(u => ids.Contains(u.Id)).Select(u => new SimplifiedUserDTO() { Id = u.Id, FirstName = u.FirstName, LastName = u.LastName, ProfilePictureUrl = u.ProfilePictureUrl} ).ToListAsync();
+        return await _dataContext.ApplicationUsers.Where(u => ids.Contains(u.Id)).Select(u => new SimplifiedUserDTO() { Id = u.Id, FirstName = u.FirstName, LastName = u.LastName, ProfilePictureUrl = u.ProfilePictureUrl }).ToListAsync();
     }
 
     public async Task<SimplifiedUserDTO?> GetSimplifiedDTOAsync(int id)
