da3fd254a204a685bfae375261f5daa6afeb392a
Ahmed Youssef <ahmed.muhammed.youssef@gmail.com>
2025-11-03 15:31:52 +0200

    test: add tests for age calculations #117

---
+++ tests/WebApp/Common/Shared.Test/Extensions/DateTimeExtensionsTests.cs
@@ -0,0 +1,33 @@
+﻿using Shared.Extensions;
+
+namespace Shared.Test.Extensions;
+
+public class DateTimeExtensionsTests
+{
+    [Fact]
+    public void CaculateAge_ShouldReturnCorrectAge_ForValidInputs()
+    {
+        // Arrange
+        int expectedAge = 25;
+        DateTime dob = DateTime.UtcNow.AddYears(-expectedAge);
+
+        // Act
+        int age = dob.CalculateAge();
+        
+        // Assert
+        Assert.Equal(expectedAge, age);
+    }
+
+    [Fact]
+    public void CaculateAge_ShouldReturnZero_ForFutureDates()
+    {
+        // Arrange
+        DateTime dob = DateTime.UtcNow.AddYears(1);
+        
+        // Act
+        int age = dob.CalculateAge();
+
+        // Assert
+        Assert.Equal(0, age);
+    }
+}
