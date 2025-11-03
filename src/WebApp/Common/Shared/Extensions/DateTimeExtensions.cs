c9c111862b8049d0fbc458c7d9bf6c7825c07597
Ahmed Youssef <ahmed.muhammed.youssef@gmail.com>
2025-11-03 15:31:30 +0200

    fix: handles edge cases and use utc time #117

--- src/WebApp/Common/Shared/Extensions/DateTimeExtensions.cs
+++ src/WebApp/Common/Shared/Extensions/DateTimeExtensions.cs
@@ -2,20 +2,34 @@
 
 public static class DateTimeExtensions
 {
+    /// <summary>
+    /// Calculates the age in years based on the specified date of birth.
+    /// </summary>
+    /// <param name="dob">The date of birth from which to calculate the age.</param>
+    /// <returns>The age in years. Returns 0 if the calculated age is negative.</returns>
     public static int CalculateAge(this DateTime dob)
     {
-        DateTime now = DateTime.Now;
+        DateTime now = DateTime.UtcNow;
         int age = now.Year - dob.Year;
         // Adjust if birthday hasn't occurred yet
         if (dob > now.AddYears(-age))
         {
             age--;
         }
-        return age;
+        return age > 0 ? age : 0;
     }
+
+    /// <summary>
+    /// Converts the specified <see cref="DateTime"/> to a human-readable relative time string.
+    /// </summary>
+    /// <param name="dateTime">The <see cref="DateTime"/> to convert to a relative time string.</param>
+    /// <returns>A string representing the relative time from the specified <paramref name="dateTime"/> to the current UTC time.
+    /// Returns "just now" if the time difference is less than one minute, or a formatted string indicating the number
+    /// of minutes, hours, or days ago. If the date is more than a week ago, returns the date formatted as "dd MMM
+    /// yyyy".</returns>
     public static string ToRelativeTimeString(this DateTime dateTime)
     {
-        var timeDiff = DateTime.Now - dateTime;
+        var timeDiff = DateTime.UtcNow - dateTime;
 
         if (timeDiff.TotalMinutes < 1)
             return "just now";
