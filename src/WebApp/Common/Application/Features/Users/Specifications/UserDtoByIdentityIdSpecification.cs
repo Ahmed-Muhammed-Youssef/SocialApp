a3fda6dede5d77a93eca4695e0865892a45b67f2
Ahmed Youssef <ahmed.muhammed.youssef@gmail.com>
2025-11-04 16:12:29 +0200

    feat: add get user dto by identity id specification #117

---
+++ src/WebApp/Common/Application/Features/Users/Specifications/UserDtoByIdentityIdSpecification.cs
@@ -0,0 +1,14 @@
+﻿using Application.Common.Mappings;
+using Domain.Entities;
+using Shared.Specification;
+
+namespace Application.Features.Users.Specifications;
+
+public class UserDtoByIdentityIdSpecification : BaseSpecification<ApplicationUser, UserDTO>
+{
+    public UserDtoByIdentityIdSpecification(string id)
+    {
+        Criteria = u => u.IdentityId == id;
+        Selector = u => UserMappings.ToDto(u);
+    }
+}
