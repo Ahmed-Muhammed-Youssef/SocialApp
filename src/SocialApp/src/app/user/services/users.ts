b49b9b07e2dd1a5be83fd42c1e06af46caf55a6b
Ahmed Youssef <ahmed.muhammed.youssef@gmail.com>
2025-12-07 11:52:34 +0200

    feat: add get user posts angular service #117

--- src/SocialApp/src/app/user/services/users.ts
+++ src/SocialApp/src/app/user/services/users.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { UserDTO } from '../../auth/models/user-dto';
 import { Observable } from 'rxjs';
+import { PostDTO } from '../../newsfeed/models/post-dto';
 
 @Injectable({
   providedIn: 'root',
@@ -14,5 +15,10 @@ export class UsersService {
   getUserById(userId : number) : Observable<UserDTO>
   {
     return this.httpClient.get<UserDTO>(`${this.baseUrl}/${userId}`)
+  }
+
+  getUserPosts(userId: number) : Observable<PostDTO>
+  {
+    return this.httpClient.get<PostDTO>(`${this.baseUrl}/${userId}/posts`)
   }
 }
