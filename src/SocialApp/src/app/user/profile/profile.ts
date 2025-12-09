cb86be8ef77c9417464904397d9d2a807d66335c
Ahmed Youssef <ahmed.muhammed.youssef@gmail.com>
2025-12-09 21:45:37 +0200

    fix: navigation error #117
    fixed issues:
    user could navigate to the chat with himself.
    user profile will not load if he moved to his profile from another user profile using router (inthis case, the nav bar)

--- src/SocialApp/src/app/user/profile/profile.ts
+++ src/SocialApp/src/app/user/profile/profile.ts
@@ -8,6 +8,7 @@ import { PostItem } from '../../newsfeed/post-item/post-item';
 import { PostDTO } from '../../newsfeed/models/post-dto';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { AuthService } from '../../auth/services/auth';
 
 @Component({
   selector: 'app-profile',
@@ -18,8 +19,10 @@ import { MatButtonModule } from '@angular/material/button';
 export class Profile {
   private userService = inject(UsersService);
   private route = inject(ActivatedRoute);
+  private authService = inject(AuthService);
 
   user = signal<UserDTO | null>(null);
+  currentUserId = signal(this.authService.getUserData()?.id);
   posts = signal<PostDTO[]>([]);
   defaultImage = 'imgs/default-user.png';
 
@@ -27,16 +30,27 @@ export class Profile {
   {
     const id = Number(this.route.snapshot.paramMap.get('id'));
 
-    this.userService.getUserById(id).subscribe({
+    this.route.paramMap.subscribe(params => {
+        const id = Number(params.get('id'));
+        this.loadUser(id);
+      });
+
+    
+  }
+
+  loadUser(id: number | null) {
+    if (id) {
+      this.userService.getUserById(id).subscribe({
       next: (res) => this.user.set(res),
       error: (err) => console.error(err)
-    });
+      });
 
-    // Get user posts
-    this.userService.getUserPosts(id).subscribe({
-      next: res => this.posts.set(res),
-      error: err => console.error(err)
-    });
+      // Get user posts
+      this.userService.getUserPosts(id).subscribe({
+        next: res => this.posts.set(res),
+        error: err => console.error(err)
+      });
+    }
   }
 
 }
