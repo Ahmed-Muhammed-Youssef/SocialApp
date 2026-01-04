872405e2ee5fbca4a0dd43a94b8275f4f3557216
Ahmed Youssef <ahmed.muhammed.youssef@gmail.com>
2026-01-04 21:46:31 +0200

    fix: options must have an empty constructor #117

--- src/API/Infrastructure/Media/Cloudinary/CloudinaryOptions.cs
+++ src/API/Infrastructure/Media/Cloudinary/CloudinaryOptions.cs
@@ -1,3 +1,9 @@
 ﻿namespace Infrastructure.Media.Cloudinary;
 
-public record CloudinaryOptions(string CloudName, string APIKey, string APISecret);
+public record CloudinaryOptions
+{
+    public required string CloudName { get; init; }
+    public required string APIKey { get; init; }
+    public required string APISecret { get; init; }
+
+}
