97eb40bcc7832dadd09de8d18bbb4ce3d822bfbb
Ahmed Youssef <ahmed.muhammed.youssef@gmail.com>
2026-01-04 15:30:36 +0200

    refact: use records for options #117

--- src/API/Infrastructure/Media/Cloudinary/CloudinaryOptions.cs
+++ src/API/Infrastructure/Media/Cloudinary/CloudinaryOptions.cs
@@ -1,8 +1,3 @@
 ﻿namespace Infrastructure.Media.Cloudinary;
 
-public class CloudinaryOptions
-{
-    public required string CloudName { get; init; }
-    public required string APIKey { get; init; }
-    public required string APISecret { get; init; }
-}
+public record CloudinaryOptions(string CloudName, string APIKey, string APISecret);
