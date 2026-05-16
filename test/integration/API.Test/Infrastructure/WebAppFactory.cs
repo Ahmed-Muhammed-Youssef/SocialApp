f002c729461e81be179ae8d759944cec0eb9b650
Ahmed.M Youssef <ahmed.muhammed.youssef@gmail.com>
2026-05-16 16:44:21 +0300

    feat: add auth integration test

---
+++ test/integration/API.Test/Infrastructure/WebAppFactory.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Testcontainers.MsSql;
+
+namespace API.Test.Infrastructure;
+
+public sealed class WebAppFactory : WebApplicationFactory<Program>, IAsyncLifetime
+{
+    private readonly MsSqlContainer _sqlContainer = new MsSqlBuilder("mcr.microsoft.com/mssql/server:2022-CU14-ubuntu-22.04")
+        .WithName("AppDb")
+        .WithPassword("Password123!")
+        .Build();
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.UseSetting("ConnectionStrings:AppConnection", _sqlContainer.GetConnectionString());
+        builder.UseEnvironment("Development"); // it's the default value, but we added it to be more explicit and to avoid any confusion when debugging tests
+
+        base.ConfigureWebHost(builder);
+    }
+
+    public async Task InitializeAsync()
+    {
+        await _sqlContainer.StartAsync();
+    }
+
+    public new async Task DisposeAsync()
+    {
+        await _sqlContainer.StopAsync();
+    }
+}
