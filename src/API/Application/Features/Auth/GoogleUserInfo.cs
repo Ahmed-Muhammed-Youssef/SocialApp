eed5058ea0158d9eaff16b7435711cb6b1f7b51f
Ahmed.M Youssef <ahmed.muhammed.youssef@gmail.com>
2026-07-25 01:30:17 +0300

    refactor: remove dead code

--- src/API/Application/Features/Auth/GoogleUserInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Application.Features.Auth;
-
-public record GoogleUserInfo
-{
-    public required string Name { get; init; }
-    public required string Email { get; init; }
-    public bool VerifiedEmail { get; init; }
-    public string? PictureUrl { get; init; }
-}
--- src/API/Application/Features/Auth/IGoogleAuthService.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Application.Features.Auth;
-
-public interface IGoogleAuthService
-{
-    Task<GoogleUserInfo> GetUserFromGoogleAsync(string code);
-}
